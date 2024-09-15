83427c4079a0aff65b4f293099d77fe2174ec18d
miguel abache <miiguel.abache@gmail.com>
2024-09-15 16:07:34 -0400

    icon sun light

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,13 +1,12 @@
 <script setup>
 import IconDarkMode from './icons/IconDarkMode.vue'
 import IconDownload from './icons/IconDownload.vue'
+import IconLigthMode from './icons/IconLightMode.vue'
 import IconTranslate from './icons/IconTranslate.vue'
 </script>
 
 <template>
-  <nav
-    class="w-full h-full flex items-center justify-between py-1 px-5 max-w-5xl xl:max-w-6xl border border-red-600"
-  >
+  <nav class="w-full h-full flex items-center justify-between py-1 px-5 max-w-5xl xl:max-w-6xl">
     <div class="w-7 h-7 bg-brand-dark flex items-center justify-center p-5 rounded-xl">
       <span class="text-white font-semibold text-base">MA</span>
     </div>
@@ -15,8 +14,10 @@ import IconTranslate from './icons/IconTranslate.vue'
       <button title="translate" class="text-xl">
         <IconTranslate />
       </button>
+      <!-- TODO :: Cambia dependiendo del tema y el title -->
       <button title="Switch to dark Mode">
         <IconDarkMode />
+        <IconLigthMode />
       </button>
       <a
         class="w-auto h-7 px-4 font-medium flex items-center gap-2 bg-brand-dark text-white rounded-3xl lg:h-10 lg:px-6 cursor-pointer active:scale-[1.1] transition-transform duration-300"
